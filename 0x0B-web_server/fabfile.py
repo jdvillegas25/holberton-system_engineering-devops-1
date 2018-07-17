41a89e282d9b524ec2f7bbe6721954a7a88b077c
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-07-17 22:31:25 +0000

    Remove ./ in the clean function

--- 0x0B-web_server/fabfile.py
+++ 0x0B-web_server/fabfile.py
@@ -13,8 +13,8 @@ def pack():
 
 def deploy():
     put("./holbertonwebapp.tar.gz", "/tmp/holbertonwebapp.tar.gz")
-    run("mkdir /tmp/holbertonwebapp")
+    run("mkdir /tmp/holbertonwebapp/")
     run("tar -zxvf /tmp/holbertonwebapp.tar.gz -C /tmp/holbertonwebapp/")
 
 def clean():
-    local("rm ./holbertonwebapp.tar.gz")
+    local("rm holbertonwebapp.tar.gz")
