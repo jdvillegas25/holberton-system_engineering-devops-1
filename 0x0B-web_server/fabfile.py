533ad0176ea18767399e3bd99a301064982b8779
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-07-17 22:33:47 +0000

    Fix PEP8 styling

--- 0x0B-web_server/fabfile.py
+++ 0x0B-web_server/fabfile.py
@@ -7,14 +7,30 @@ from fabric.api import *
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/holberton'
 
+
 def pack():
+    '''
+    Packages the current directory into a .tar.gz archive
+    '''
+
     local("tar --exclude='*.tar.gz' -zcvf holbertonwebapp.tar.gz .")
 
 
 def deploy():
+    '''
+    Deploys the archive onto the remote server in /tmp/,
+    creates a holbertonwebapp folder, and extracts the
+    archive contents into this directory
+    '''
+
     put("./holbertonwebapp.tar.gz", "/tmp/holbertonwebapp.tar.gz")
     run("mkdir /tmp/holbertonwebapp/")
     run("tar -zxvf /tmp/holbertonwebapp.tar.gz -C /tmp/holbertonwebapp/")
 
+
 def clean():
+    '''
+    Removes the archive from the local machine
+    '''
+
     local("rm holbertonwebapp.tar.gz")
