be65af47e8a567e2ad4f9e515965f4ee7cdfd96e
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-07-17 22:47:35 +0000

    Change docstring quotes from single to double

--- 0x0B-web_server/fabfile.py
+++ 0x0B-web_server/fabfile.py
@@ -1,6 +1,6 @@
-'''
+"""
 Fabric file for transferring a file to a remote server from local machine
-'''
+"""
 
 from fabric.api import *
 env.user = 'ubuntu'
@@ -8,19 +8,19 @@ env.key_filename = '~/.ssh/holberton'
 
 
 def pack():
-    '''
+    """
     Packages the current directory into a .tar.gz archive
-    '''
+    """
 
     local("tar --exclude='*.tar.gz' -cvzf holbertonwebapp.tar.gz .")
 
 
 def deploy():
-    '''
+    """
     Deploys the archive onto the remote server in /tmp/,
     creates a holbertonwebapp folder, and extracts the
     archive contents into this directory
-    '''
+    """
 
     put("holbertonwebapp.tar.gz", "/tmp/")
     run("mkdir /tmp/holbertonwebapp/")
@@ -28,8 +28,8 @@ def deploy():
 
 
 def clean():
-    '''
+    """
     Removes the archive from the local machine
-    '''
+    """
 
     local("rm holbertonwebapp.tar.gz")
