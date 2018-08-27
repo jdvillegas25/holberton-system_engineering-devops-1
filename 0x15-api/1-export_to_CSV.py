9135a8ace7f41445e1ea3122a63bdf839a39f76b
Derek <dhklangs@gmail.com>
2018-08-27 22:22:10 +0000

    Add task 1 which exports data from the API to a csv file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Pings a To-Do API for data and displays tasks completed
+and tasks done for a specified user
+"""
+import csv
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    employee_id = argv[1]
+    url_todo = 'https://jsonplaceholder.typicode.com/todos/'
+    url_user = 'https://jsonplaceholder.typicode.com/users/'
+    todo = requests.get(url_todo, params={'userId': employee_id})
+    user = requests.get(url_user, params={'id': employee_id})
+
+    todo_dict_list = todo.json()
+    user_dict_list = user.json()
+
+    employee = user_dict_list[0]['name']
+
+    with open("{}.csv".format(employee_id), "a+") as csvfile:
+        csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todo_dict_list:
+            status = task['completed']
+            title = task['title']
+            csvwriter.writerow(["{}".format(employee_id),
+                                "{}".format(employee),
+                                "{}".format(status),
+                                "{}".format(title)])
