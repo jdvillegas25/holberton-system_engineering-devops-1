731d4bded99002f0bd26ce22c4c93e96fea1d130
Derek <dhklangs@gmail.com>
2018-08-30 22:49:41 +0000

    Add task 1 which returns the titles of the top 10 titles in a subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module that displays the 10 hot reddit posts in a specified subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+        Queries the Reddit API and prints the titles of the top 10 posts
+        for a subreddit
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    user_agent = {'User-Agent': 'Derek@holberton'}
+    req = requests.get(url, headers=user_agent, allow_redirects=False,
+                       params={'limit': 10})
+    if req.status_code == 200:
+        req = req.json()
+        data = req.get('data')
+        children = data.get('children')
+        if data is not None and children is not None:
+            posts = ""
+            for post in children:
+                post_data = post.get('data')
+                title = post_data.get('title')
+                print(title)
+    else:
+        print('None')
