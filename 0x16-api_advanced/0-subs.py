4ed169ad6e6c45aa58b714caa2221ba8efbb004b
Derek <dhklangs@gmail.com>
2018-08-30 21:28:12 +0000

    Add README to be updated later and task 0 which returns the number of subscribers to a subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module for a function that returns the number of subscribers in a subreddit
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+        Returns the number of subscribers to a specified subreddit
+    """
+    url = 'https://www.reddit.com/r/{}/about/.json'.format(subreddit)
+    req = requests.get(url).json()
+    data = req.get('data')
+    return data.get('subscribers')
+
